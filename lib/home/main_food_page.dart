dd326d7f0ae70b614e0f21af08614319f1ea67c7
Vipul Kumawat <vipullkumawat2000@gmail.com>
2022-03-18 21:46:50 +0530

    D1

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/home/food_page_body.dart';
 import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({Key? key}) : super(key: key);
@@ -23,33 +26,30 @@ class _MainFoodPageState extends State<MainFoodPage> {
                 children: [
                   Column(
                     children: [
-                      Text('Country'),
-                      Text('City'),
+                      BigText(text: "India", color: AppColors.mainColor,),
+                      Row(
+                        children: [
+                          SmallText(
+                            text: "Rajasthan", color: Colors.black54,),
+                          Icon(Icons.arrow_drop_down_rounded),
+                        ],
+                      )
                     ],
                   ),
-                  Center(
-                    child: Container(
-                      width: 45,
-                      height: 45,
-                      child: Icon(Icons.search, color: Colors.white,),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: AppColors.mainColor,
-                      ),
+                  Container(
+                    width: 45,
+                    height: 45,
+                    child: Icon(Icons.search, color: Colors.white,),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
                     ),
                   )
                 ],
               ),
             ),
-
-
-
-
-
-
-
-
           ),
+          FoodPageBody()
         ],
       )
     );
