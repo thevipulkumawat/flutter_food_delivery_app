9e31698f401b6476efd871c5ce4228ed9871cae8
Vipul Kumawat <vipullkumawat2000@gmail.com>
2022-03-19 22:01:35 +0530

    D2

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/home/food_page_body.dart';
 import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
 
@@ -14,13 +15,15 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
+    //print("current height is "+MediaQuery.of(context).size.width.toString());
     return Scaffold(
       body:Column(
         children: [
+          // header
           Container(
             child: Container(
-              margin: EdgeInsets.only(top: 45, bottom: 15),
-              padding: EdgeInsets.only(left: 20, right: 20),
+              margin: EdgeInsets.only(top: Dimensions.height45, bottom: Dimensions.height15),
+              padding: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -37,11 +40,11 @@ class _MainFoodPageState extends State<MainFoodPage> {
                     ],
                   ),
                   Container(
-                    width: 45,
-                    height: 45,
-                    child: Icon(Icons.search, color: Colors.white,),
+                    width: Dimensions.height45,
+                    height: Dimensions.height45,
+                    child: Icon(Icons.search, color: Colors.white, size: Dimensions.iconSize24,),
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
+                      borderRadius: BorderRadius.circular(Dimensions.radius15),
                       color: AppColors.mainColor,
                     ),
                   )
@@ -49,6 +52,7 @@ class _MainFoodPageState extends State<MainFoodPage> {
               ),
             ),
           ),
+          //body
           FoodPageBody()
         ],
       )
---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,38 @@
+import 'package:get/get.dart';
+class Dimensions{
+
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+  static double pageView = screenHeight/2.28;
+  static double pageViewContainer = screenHeight/3.32;
+  static double pageViewTextContainer = screenHeight/6.1;
+
+  //dynamic height padding and margin
+
+  static double height10 = screenHeight/73.2;
+  static double height15 = screenHeight/48.8;
+  static double height20 = screenHeight/36.6;
+  static double height30 = screenHeight/24.4;
+  static double height45 = screenHeight/16.26;
+
+  //dynamic width padding and margin
+
+  static double width10 = screenHeight/73.2;
+  static double width15 = screenHeight/48.8;
+  static double width20 = screenHeight/36.6;
+  static double width30 = screenHeight/24.4;
+  static double width45 = screenHeight/16.26;
+
+  static double font20 = screenHeight/36.6;
+
+  //radius
+  static double radius15 = screenHeight/48.8;
+  static double radius20 = screenHeight/36.6;
+  static double radius30 = screenHeight/24.4;
+
+  //icon size
+  static double iconSize24 = screenHeight/30.5;
+
+
+}
